5bc9a97d18dbeb40d79279094cea1548d703e60c
Alexander Shibisty <alexander.shibisty@postindustria.com>
2020-01-22 14:50:26 +0200

    Added serverListenCallback and binaryTypes params to main function

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 
 const { createServer, proxy } = require('aws-serverless-express');
 
-module.exports = app => (event, ctx) => { proxy(createServer(app.callback()), event, ctx); }
+module.exports = (app, serverListenCallback, binaryTypes) => {
+    return (event, ctx) => {
+        proxy(createServer(app.callback(), serverListenCallback, binaryTypes), event, ctx);
+    }
+}
